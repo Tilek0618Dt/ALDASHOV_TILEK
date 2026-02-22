dadf958a7fd060773f68fad71171f52b2311b753
Tilek0618Dt <milardermilon@gmail.com>
2026-02-22 18:35:02 +0300

    Update vip.py

--- app/handlers/vip.py
+++ app/handlers/vip.py
@@ -1,1 +1,9 @@
+from aiogram import Router, F
+from aiogram.types import CallbackQuery
 
+router = Router()
+
+@router.callback_query(F.data == "menu:vip")
+async def vip(c: CallbackQuery):
+    await c.message.answer("🎥 VIP меню (кейин толтурабыз)")
+    await c.answer()
