a1f99477c5e98005017eb717730bf19e72c64f9e
Tilek0618Dt <milardermilon@gmail.com>
2026-02-22 18:31:24 +0300

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,1 +1,10 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from app.keyboards import main_menu_kb
 
+router = Router()
+
+@router.message(CommandStart())
+async def start(m: Message):
+    await m.answer("Салам досум! Мен сенин AI ботум 😎💎", reply_markup=main_menu_kb())
