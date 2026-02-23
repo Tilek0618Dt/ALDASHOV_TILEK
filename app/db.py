c1cb59d5ae1ddf9b6aa2ed80adafab58649fcd5f
Tilek0618Dt <milardermilon@gmail.com>
2026-02-23 17:11:14 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,15 +1,1 @@
-# app/db.py
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlalchemy.orm import declarative_base
-from app.config import DATABASE_URL
 
-Base = declarative_base()
-
-# DATABASE_URL мисалы: postgresql+asyncpg://user:pass@host:5432/db
-ENGINE = create_async_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
-
-SessionLocal = async_sessionmaker(
-    bind=ENGINE,
-    class_=AsyncSession,
-    expire_on_commit=False,
-)
