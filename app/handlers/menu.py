82575ae028a98baebe1a040aacbdddc68b4a7dd0
Tilek0618Dt <milardermilon@gmail.com>
2026-02-23 17:08:29 +0300

    Update menu.py

--- app/handlers/menu.py
+++ app/handlers/menu.py
@@ -1,10 +1,1 @@
-from aiogram import Router, F
-from aiogram.types import CallbackQuery
-from app.keyboards import main_menu_kb
 
-router = Router()
-
-@router.callback_query(F.data.startswith("menu:"))
-async def menu(c: CallbackQuery):
-    await c.message.answer("Меню ✅", reply_markup=main_menu_kb())
-    await c.answer()
