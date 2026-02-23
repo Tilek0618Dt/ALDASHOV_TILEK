80c8ef6c1efcf5d4476130de64097f21556ce612
Tilek0618Dt <milardermilon@gmail.com>
2026-02-23 17:09:04 +0300

    Update premium.py

--- app/handlers/premium.py
+++ app/handlers/premium.py
@@ -1,9 +1,1 @@
-from aiogram import Router, F
-from aiogram.types import CallbackQuery
 
-router = Router()
-
-@router.callback_query(F.data == "menu:premium")
-async def premium(c: CallbackQuery):
-    await c.message.answer("⭐ Premium меню (кейин толтурабыз)")
-    await c.answer()
